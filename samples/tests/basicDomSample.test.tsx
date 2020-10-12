fbfa930f3104aca5b7fbeb4d2e1c7e2de0787ffb
Michal Kočárek <michal.kocarek@merck.com>
2020-10-13 01:56:18 +0200

    Improve usability of basic test

--- samples/tests/basicDomSample.test.tsx
+++ samples/tests/basicDomSample.test.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { LoginScreen } from "./components/loginScreen";
 
 describe("Component with static fields", () => {
   it("should render correctly", async () => {
     render(<LoginScreen />);
 
-    const element = await screen.findByText(/User:/);
+    const user = await screen.findByLabelText(/User:/);
+    await userEvent.type(user, "john.doe");
+
+    const password = await screen.findByLabelText(/Password:/);
+    await userEvent.type(password, "123456");
+
+    const checkbox = await screen.findByLabelText(
+      /I accept terms and conditions/
+    );
+    userEvent.click(checkbox);
+
+    const submitButton = await screen.findByText(/Login/);
+    userEvent.click(submitButton);
+
+    await screen.findByText(/Login failed./);
 
     screen.debug();
-
-    expect(element).not.toBeNull();
-  });
+  }, 50_000);
 });
--- samples/tests/components/loginScreen.tsx
+++ samples/tests/components/loginScreen.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { FormEventHandler, useState } from "react";
 
 export const LoginScreen: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setIsError] = useState("");
+
+  // This is very fake error handler, that simulates asynchronous action and few
+  // changes in the DOM tree itself...
+  const submitForm: FormEventHandler = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsError("Login failed. Wrong credentials were supplied.");
+      setIsLoading(false);
+    }, 5000);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
+      {error && <div style={{ color: "red" }}>{error}</div>}
       <div>
         <label htmlFor="username">User:</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" id="username" />
       </div>
       <div>
         <label htmlFor="password">Password:</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" id="password" />
       </div>
       <div>
-        <input type="checkbox" name="accept" />
+        <input type="checkbox" name="accept" id="accept" />
         <label htmlFor="accept">I accept terms and conditions</label>
+      </div>
+      <div>
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Logging in..." : "Login"}
+        </button>
       </div>
     </form>
   );
