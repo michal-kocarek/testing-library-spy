78b258a995aa834eb0004240d94c35b4cf6debb3
Michal Kočárek <michal.kocarek@merck.com>
2020-10-13 00:35:30 +0200

    Rename sample to reflect its purpose

--- samples/tests/basicDomSample.test.tsx
+++ samples/tests/basicDomSample.test.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { LoginScreen } from "./components/loginScreen";
 
-describe("foo", () => {
-  it("should bar", async () => {
+describe("Component with static fields", () => {
+  it("should render correctly", async () => {
     render(<LoginScreen />);
-    // TODO: Make it update already after first render!
 
     const element = await screen.findByText(/User:/);
 
-    fireEvent.click(await screen.getByRole("checkbox"));
+    screen.debug();
 
     expect(element).not.toBeNull();
-    screen.debug();
   });
 });
